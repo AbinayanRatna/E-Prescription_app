b73f642af875d5c075bf50403e6ce94cbd0838a2
FathimaNusha <146192217+FathimaNusha@users.noreply.github.com>
2024-09-25 19:16:17 +0530

    login_screen updated.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -19,35 +19,45 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // form state
+  // Form state 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  // Variables 
   String? _phoneNumber;
   String? _password;
   var userBox=Hive.box<UserDetails>('User');
 
+  // Handling sign-in process
   Future<void> signIn(BuildContext context,String userType) async {
+    //reference to the firebase db based on user type
     DatabaseReference dbref=FirebaseDatabase.instance.ref().child("${userType}/${_phoneNumber}");
+    //fetch account from firebase
     DatabaseEvent accountEvent=await dbref.once();
     DataSnapshot accountSnapshot=accountEvent.snapshot;
+    //checking account is exist 
     if(accountSnapshot.value != null){
+      // fetch password and username 
       DatabaseEvent passwordEvent=await dbref.child("password").once();
       DatabaseEvent nameEvent=await dbref.child("name").once();
       DataSnapshot passwordSnapshot=passwordEvent.snapshot;
       DataSnapshot nameSnapshot=nameEvent.snapshot;
+      //Check if the password is correct
       if(_password == passwordSnapshot.value){
         if(userType=="doctor"){
           UserDetails userOfApp=UserDetails(user_phone: _phoneNumber!, user_type: "doctor", user_logout: false,userHospitalNow: "No hospital",userName:nameSnapshot.value.toString() );
           userBox.add(userOfApp);
           Navigator.push(context, MaterialPageRoute(builder: (context)=>DocHomescreen(phoneNumber: _phoneNumber!,)));
-        }else if(userType=="patient"){
+        }
+        else if(userType=="patient"){
           UserDetails userOfApp=UserDetails(user_phone: _phoneNumber!, user_type: "patient", user_logout: false,userHospitalNow: "No hospital",userName:nameSnapshot.value.toString());
           userBox.add(userOfApp);
           Navigator.push(context, MaterialPageRoute(builder: (context)=>PatHomeScreen(phoneNumber: _phoneNumber!,)));
         }
-      }else{
+      }
+      else{
         _showErrorDialog(context, "Incorrect Password");
       }
-    }else{
+    }
+    else{
       _showErrorDialog(context, "Please create an account first");
     }
   }
